1b7014261481cbc9d918407ca5fb09ebaa8908b8
Aya  BEN BRAHIM <ayabenbrahim4@gmail.com>
2021-10-24 22:03:43 +0200

    update exo3

--- TD3/exercice3/src/Main.java
+++ TD3/exercice3/src/Main.java
@@ -1,9 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+Map<Point, Circle> m = new HashMap<>();
+Point p1 = new Point(100,150);
+Circle c1 = new Circle(p1,100);
+m.put(p1, c1);
+System.out.println(m.containsKey(p1)); // true
+
+System.out.println(m.containsKey(new Point(1,2))); //false
 	}
 
 }
